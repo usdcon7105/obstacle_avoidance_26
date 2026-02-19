5936f3dd5b1772f0f8034da28115982b2af1575e
dtruong44 <dtruong@sandiego.edu>
2026-02-19 15:05:15 -0800

    added userDefault handler getters and setters

---
+++ obstacle_avoidance/Data/UserDefaultsHandler.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+class UserDefaultsHandler {
+
+    static let shared = UserDefaultsHandler()
+    private let defaults = UserDefaults.standard
+
+    // Measurement Type, User Height, Haptic, Location Share
+
+
+    func setMeasurementType(type: String) {
+        defaults.set(type, forKey: "measurement_type")
+    }
+
+    func setUserHeight(height: Double) {
+        defaults.set(height, forKey: "user_height")
+    }
+
+    func setHapticFeedback(enabled: Bool) {
+        defaults.set(enabled, forKey: "haptic_feedback")
+    }
+
+    func setLocationSharing(enabled: Bool) {
+        defaults.set(enabled, forKey: "location_sharing")
+    }
+
+    func getMeasurementType(type: String) {
+        return defaults.object(forKey: "measurement_type")
+    }
+
+    func getUserHeight(height: Double) {
+        return defaults.object(forKey: "user_height")
+    }
+
+    func getHapticFeedback(enabled: Bool) {
+        return defaults.object(forKey: "haptic_feedback")
+    }
+
+    func getLocationSharing(enabled: Bool) {
+        return defaults.object(forKey: "location_sharing")
+    }
+}
